820e43670244ab6f986f66cc7dc3d26a04868f46
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2022-02-21 16:01:27 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variaveis ponto flutuate");
+
+            double salario;
+            salario = 1200.70;
+                Console.WriteLine(salario);
+
+            double idade;
+            idade = 15.0 / 2;
+            Console.WriteLine(idade);
+
+            idade = 5.0 / 3;
+            Console.WriteLine(idade);
+
+            Console.WriteLine(" a execução acabou. Tecle enternpara sair...");
+            Console.ReadLine();
+
+        }
+    }
+}
